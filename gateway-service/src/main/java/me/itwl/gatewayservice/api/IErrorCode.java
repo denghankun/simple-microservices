e5613ab46e90497a6ef2e953e0b8604b987bdf0d
dhkun <414147674@qq.com>
2020-08-16 22:33:40 +0800

    失败返回异常

---
+++ gateway-service/src/main/java/me/itwl/gatewayservice/api/IErrorCode.java
@@ -0,0 +1,7 @@
+package me.itwl.gatewayservice.api;
+
+public interface IErrorCode {
+    long getCode();
+
+    String getMessage();
+}
--- gateway-service/src/main/java/me/itwl/gatewayservice/config/ResourceServerConfigurer.java
+++ gateway-service/src/main/java/me/itwl/gatewayservice/config/ResourceServerConfigurer.java
@@ -1,5 +1,6 @@
 package me.itwl.gatewayservice.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -8,16 +9,27 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 /**
  * Created by Steven on 2019/10/27.
  */
+@AllArgsConstructor
 @EnableWebFluxSecurity
 public class ResourceServerConfigurer {
+
+    private final RestfulAccessDeniedHandler restfulAccessDeniedHandler;
+    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http.authorizeExchange()
                 .pathMatchers("/actuator/**").permitAll()
-                .anyExchange().authenticated();
+                .anyExchange().authenticated()
+                .and().exceptionHandling()
+                    .accessDeniedHandler(restfulAccessDeniedHandler)
+                    .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                    .csrf().disable()
+        ;
 
         http.oauth2ResourceServer().jwt();
+        http.oauth2ResourceServer().authenticationEntryPoint(restAuthenticationEntryPoint);
 
         return http.build();
     }
